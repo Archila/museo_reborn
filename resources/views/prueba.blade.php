9d66a8bb3d1aa88f9514fe6935cd78ad2065a40b
Yordan <yorchdlacruz@gmail.com>
2017-09-29 20:43:05 -0600

    Borrando prueba 1.0

--- resources/views/prueba.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.home')
-@section('title', 'AgregarPieza')
-@section('barra')
-<ul class="section table-of-contents">
-  <li><a href="#historia">Historica</a></li>
-  <li><a href="#pinturas">Pinturas</a></li>
-  <li><a href="#ferrocaril">Ferrocaril</a></li>
-</ul>
-@endsection
