0768118851b3af8be39cd796c5d43861636a0abc
Ivan Esteban Archila <ivan@debian.debian.org>
2017-10-27 13:51:32 -0600

    RegisterController

--- museo/app/Http/Controllers/Auth/RegisterController.php
+++ museo/app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'empleado' =>$data['empleado'],
+            'empleado' =>1,
         ]);
     }
 }
