c2c7003265c389cbd01656b3fe6b79ade99a8322
Museo Historia Xela <museohistoriasbx@gmail.com>
2017-10-28 23:20:39 +0000

    index.blade.php edited online with Bitbucket boletos view

--- museo/resources/views/boletos/index.blade.php
+++ museo/resources/views/boletos/index.blade.php
@@ -1,12 +1,9 @@
 @extends('templates.home')
 
 @section('content')
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-
-
-
-    <div class="row">
-
+ 
+    <div class="row ">
+      
       <div class="col s6 center-align ">
         <p class="center-align caption">Tipo de visitante</p>
           @foreach ($visitante as $v)
@@ -22,77 +19,40 @@
             <label for="{{$r->nombre}}">{{$r->nombre}}</label>
           @endforeach
       </div>
-
+    
     </div>
+   
 
-
-  <div class="row">
-      <div class="col s6">
-        <div class="row">
+  <div class="row ">
+      <div class="col s7 m7 ">
+        
+        <div class="row ">
+          <form id="formValidate" name="boleto">
           <div class="col s12">
             <h5 class="light">Precio:</h5>
           </div>
-
+         
           <div class="col s12" id='town'>
-
+           
           </div>
-
-        </div>
-        <div class="row">
-          <form name="boleto">
             <input type="hidden" name="_token" value="{{csrf_token()}}" id="token">
             <div class="input-field col s10 center-align">
               <input value="1" type="hidden" id="ingreso2" onKeyUp="boleteria()" type="number" class="validate" placeholder="Cantidad">
             </div>
-            <div class="input-field col s10 center-align">
-              <input id="ingreso3" onKeyUp="nombre()" type="text" class="validate" placeholder="Nombre">
+            <div class="input-field col s8 center-align">
+              <input id="ingreso3" onKeyUp="nombre()" type="text" class="validate" autocomplete="off" data-length="30">
+              <label for="ingreso3" data-error="incorrecto" data-success="correcto">Nombre</label>
             </div>
-            <div class="input-field col s10 center-align">
+            <div class="input-field col s0 center-align">
               <input id="ingreso4" type="hidden" name="ingreso4" value="">
             </div>
-            <div class="input-field col s6 left-align">
-
-            </div>
+            
           </form>
-          <button id="action" class="btn waves-effect waves-light" type="submit" name="action">Enviar</button>
-          <button id="add" class="btn waves-effect waves-light" type="submit" name="add">Agregar</button>
-        </div>
-      </div>
-
-    <div class="col s6 m6">
-      <div class="card hoverable z-depth-2">
-        <div class="card-content">
-        <div class="card-image">
-          <img style="margin-top:-1cm;margin-bottom:-0.5cm;" src="{{URL::asset('images/museologo.png')}}">
-        </div>
-
-          <p id="date" class="caption center-align"></p>
-        </div>
-        <div class="row ">
-        <div class="col s6 offset-s3 divider"></div>
-        </div>
-
-          <div class="row">
-              <div class="col s6">
-                <h5 class="light right-align">Total:</h5>
-              </div>
-              <div class="col s6">
-                <h5 class="light"><div id="resultado" name="resultado">Q. 0</div></h5>
-              </div>
+          <div class="col s4 center">
+            <button id="add" class="btn-floating btn-large  waves-effect waves-light  light-blue darken-4 tooltipped" data-position="bottom" data-delay="50" data-tooltip="Agregar" type="submit" name="add"><i class="material-icons">done</i></button>    
           </div>
-          <div class="row">
-              <div class="col s6 offset-s3">
-                <h5 class="light center-align"><div id="resultado2" name="resultado2">Nombre</div></h5>
-              </div>
-          </div>
-            <p class="center-align">www.museodehistoriaxela.com</p>
-          <br>
-          </div>
-
-  </div>
-
-  <div class="row">
-    <div class="col s10 offset-s1">
+          
+          <div class="col s12 center">
       <table id="tableboleto" class="bordered highlight responsive-table">
           <thead>
             <tr>
@@ -100,13 +60,13 @@
                 <th>Tarifa</th>
                 <th>Nombre</th>
                 <th>Subtotal</th>
-                <th>Accion</th>
+                <th></th>
             </tr>
           </thead>
 
           <tbody id="content_table">
             <tr>
-
+              
             </tr>
           </tbody>
           <tfoot>
@@ -120,11 +80,52 @@
           </tfoot>
         </table>
     </div>
-
+        </div>
+      </div>
+      <!-- -->
+    
+    <div class="col s5 m5 ">
+      <div class="card hoverable z-depth-2">
+        <div class="card-content">
+        <div class="card-image">
+          <img style="margin-top:-1cm;margin-bottom:-0.5cm;" src="{{URL::asset('Images/museologo.png')}}">
+        </div>
+          
+          <p id="date" class="caption center-align"></p>
+        </div>
+        <div class="row">
+        <div class="col s6 offset-s3 divider"></div>
+        </div>
+        
+          <div class="row">
+              <div class="col s6">
+                <h5 class="light right-align">Total:</h5>
+              </div>
+              <div class="col s6">
+                <h5 class="light"><div id="resultado" name="resultado">Q. 0.0</div></h5>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col s6 offset-s3">
+                <h5 class="light center-align"><div id="resultado2" name="resultado2">Visitante</div></h5>
+              </div>
+          </div>
+          <div class="row">
+          <div class="col s12 center">
+            <button id="action" class="btn  waves-effect waves-light  light-blue darken-4" type="submit" name="action"><i class="material-icons left">done_all</i>Enviar</button>       
+          </div></div>
+          <br>
+          </div>
+      
   </div>
 
+  <div class="row">
+    
+  
+  </div>
+    
 </div>
-
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>    
 <script type="text/javascript">
   var fecha,tarifa,usuario;
   var total = 0;
@@ -149,7 +150,7 @@
                     }
 
                     $(this).css("background-color", "#ECF8E0");
-
+                
                 })
                 $.ajax({
                   url: '{!!URL::to('boletos')!!}',
@@ -170,10 +171,10 @@
       });
                // alert(campo1 + ' - ' + campo2 + ' - ' + campo3+ ' - ' + campo4);
             })
-
-
-
-
+     
+    
+      
+    
   });
 
 $(document).ready(function(){
@@ -198,7 +199,7 @@ $(document).ready(function(){
       var dato3 = tarifa;
       var dato4 = usuario;
       var dato2 = parseFloat(total);
-
+    	
       var name_table=document.getElementById("tableboleto");
       var row = name_table.insertRow(0+1);
 
@@ -207,7 +208,7 @@ $(document).ready(function(){
       var cell3 = row.insertCell(2);
       var cell4 = row.insertCell(3);
       var cell5 = row.insertCell(4);
-
+      
 
       cell1.innerHTML = '<p >'+fecha+'</p>';
       cell2.innerHTML = '<p >'+tarifa+'</p>';
@@ -217,8 +218,10 @@ $(document).ready(function(){
 
       //Para calcular los totales enviando los parametros
       calculateTotals(total, 1);
+      document.getElementById("ingreso3").value = '';
+      
       //Para calcular los totales sin enviar los parametros, solo adquiriendo los datos de la columna con mismo tipo de datos
-
+    
 }
 function getRowSelected(objectPressed){
 	//Obteniendo la linea que se esta eliminando
@@ -228,30 +231,35 @@ function getRowSelected(objectPressed){
 	var tarifa=a.getElementsByTagName("td")[1].getElementsByTagName("p")[0].innerHTML;
 	var usuario=a.getElementsByTagName("td")[2].getElementsByTagName("p")[0].innerHTML;
 	var total=a.getElementsByTagName("td")[3].getElementsByTagName("p")[0].innerHTML;
-
+	
 	var array_fila = [fecha, tarifa, usuario,total];
 
 	return array_fila;
 	//console.log(numero+' '+codigo+' '+descripcion);
 }
 function calculateTotals(total,accion){
-
+	
 	var t_total=parseFloat(document.getElementById("total_total").innerHTML);
-
+  var t_total2=parseFloat(document.getElementById("resultado").innerHTML);
+  
 	//accion=1		Sumarle al los totales
 	//accion=2		Restarle al los totales
 	if (accion==1) {
-
+		
 		document.getElementById("total_total").innerHTML=parseFloat(t_total)+parseFloat(total);
+    document.getElementById("resultado").innerHTML=parseFloat(t_total)+parseFloat(total);
+    
 	}else if(accion==2){
-
+		
 		document.getElementById("total_total").innerHTML=parseFloat(t_total)-parseFloat(total);
+    document.getElementById("resultado").innerHTML=parseFloat(t_total)-parseFloat(total);
+    
 	}else{
 		alert('Accion Invalida');
 	}
 }
 function calculateTotalsBySumColumn(){
-
+	
 	var totales_n=0;
 	var array_totalesn=document.getElementsByName("total_f[]");
 	for (var i=0; i<array_totalesn.length; i++) {
@@ -267,28 +275,28 @@ function calculateTotalsBySumColumn(){
   document.getElementById("date").innerHTML = (day + "/" + month + "/" + year);
   var prod_id="";
   fecha= (year + "/" + month + "/" + day);
-
+  
 
 	$(document).on('change','.visitante',function(event){
-
+		
     $(document).on('change','.rango',function () {
-
+    
       prod_id=$(this).val();
       var a=$(this).parent();
-
+      		
 			$.get("/find/"+event.target.value+""+prod_id, function(response,state){
 			console.log(response);
-
+			
 			$("#town").empty();
 			for(i=0;i<response.length;i++)
       {
-			  $("#town").append("<input name='grupo' onClick='boleteria()' type='radio' value='"+response[i].monto+"' id='"+response[i].id+"'>"+"<label for='"+response[i].id+"'>"+response[i].monto+"</label></p>");
-
+			  $("#town").append("<input class='with-gap' name='grupo' onClick='boleteria()' type='radio' value='"+response[i].monto+"' id='"+response[i].id+"'>"+"<label for='"+response[i].id+"'>"+response[i].monto+"</label></p>");
+        
       }
 		});
 
 	});
-
+		
 	});
 
 });
@@ -311,7 +319,7 @@ function boleteria()
       {
 	      ingreso1 = (isNaN(parseInt(ingreso1)))? 0 : parseInt(ingreso1);
 		    ingreso2 = (isNaN(parseInt(ingreso2)))? 0 : parseInt(ingreso2);
-		    document.getElementById('resultado').innerHTML = "Q." + ingreso1*ingreso2;
+		    //document.getElementById('resultado').innerHTML = "Q." + ingreso1*ingreso2;
         total = ingreso1*ingreso2;
 	    }
       catch(e) {}
