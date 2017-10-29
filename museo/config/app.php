1637fc7adaa76c1725b2166d18b549a842c4d5df
Ivan Archila <ivanestarchila@gmail.com>
2017-10-29 11:58:54 -0600

    Primer prueba, referencias a Dompdf

--- museo/config/app.php
+++ museo/config/app.php
@@ -164,8 +164,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         UxWeb\SweetAlert\SweetAlertServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
-        
+        Barryvdh\DOMPDF\ServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -231,7 +231,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Alert' => UxWeb\SweetAlert\SweetAlert::class,
-        'PDF' => 'Barryvdh\DomPDF\Facade',
+        'PDF' => 'Barryvdh\DOMPDF\Facade',
 
     ],
 
