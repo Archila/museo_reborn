e1b47995d2564dc13cf43f2da7075b8a85cd2047
Ivan <ivanestarchila@gmail.com>
2017-11-16 07:38:40 -0600

    Pruebas en la tablet de la vista de qr error12

--- museo/resources/views/asistente/no_qr.blade.php
+++ museo/resources/views/asistente/no_qr.blade.php
@@ -71,9 +71,11 @@
       <p class="error">QR inválido</p>
     </article>
     <div class="row">
-      <div class="col m9 offset-m2">
-        <span class="white-text" style="font-size: 3rem;"><b>Volver a escanear</b>
-                <i class="material-icons large white-text right">arrow_forward</i></span>
+      <div class="col m8 offset-m2">
+        <span class="white-text" style="font-size: 3rem;"><b>Volver a escanear</b></span>        
+      </div>
+      <div class="col m1">
+        <i class="material-icons large white-text right">arrow_forward</i>
       </div>
     </div>
 
