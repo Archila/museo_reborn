5eea648cbc82592b38abdbee25967bbc25af7438
John Cua <cuatumax@gmail.com>
2018-01-10 23:44:11 -0600

    arreglo de ruta de creacion de eventos

--- museo/resources/views/evento/create.blade.php
+++ museo/resources/views/evento/create.blade.php
@@ -1,28 +1,19 @@
 @extends('templates.home')
   @section('content')
     <div class="container">
-
       <div class="row">
-
         <div class="center"><br>
-
           <h5 class="light">Registro de Eventos</h5>
-
         </div>
-
       </div>
     <div class="card z-depth-4">
-
       <div class="card-image">
-
         <a href="{{route('Evento.index')}}" class="btn-floating halfway-fab tooltipped waves-effect waves-light  light-blue darken-4" data-position="bottom" data-delay="50" data-tooltip="Regresar"><i class="material-icons">arrow_back</i></a>
-
       </div>
       <div class="container">
-
 			  <div class="row">
 
-          <form id="formValidate" method="POST" action="#"><br>
+          <form id="formValidate" method="POST" action="{{route('Evento.store')}}"><br>
            {{ csrf_field() }}
            {{ method_field('POST') }}
           <div class="row">
